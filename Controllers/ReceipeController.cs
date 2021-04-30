914b4d34abbeb18ca682192c4acbf429dee7c1ad
KRNagarjun <kr.nagarjun@ltts.com>
2021-04-30 10:51:03 +0530

    WEB API DEMO

---
+++ Controllers/ReceipeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceipeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetDishes([FromQuery]int Count)
+        {
+            string[] dishes = { "Idli", "Dosa", "Pongal" };
+            if (dishes.Any())
+            {
+                return NotFound();
+            }
+            return Ok(dishes.Take(Count));
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteReceipes()
+        {
+            bool badhappend = false;
+            if (badhappend)
+                return BadRequest();
+
+            return NoContent();
+        }
+    }
+}
